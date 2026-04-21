8cb99137f67d1dfbc453467333d02cee30f418d0
Yuliana Lopez <lopezrochayuli994@gmail.com>
2026-04-21 01:18:30 -0600

    test cors abierto

--- src/server.js
+++ src/server.js
@@ -23,7 +23,8 @@ const app = express();
 app.use(helmet());
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173",
+  "https://zonegym-frontend-finalyuliana.vercel.app"],
   credentials: true
 }));
 
